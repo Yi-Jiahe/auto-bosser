51b2daad3d20d6953a32a0f28d0e1273fffb6942
Jiahe <yi.jiahe00@gmail.com>
2022-04-05 09:23:05 +0800

    Add reset button

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,15 @@ import './App.css';
 import { Game } from './features/game/Game';
 
 function App() {
+  function onClick() {
+    localStorage.removeItem("persist:auto-bosser");
+    // eslint-disable-next-line no-restricted-globals
+    location.reload();
+  }
+
   return (
     <div className="App">
+      <button onClick={() => onClick()}>Reset</button>
       <Game />
     </div>
   );
