c18d41a528fc2c60f5394bd562c9af90d31f312f
Jiahe <yi.jiahe00@gmail.com>
2022-04-05 09:10:40 +0800

    Add redux-persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,38 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import expertiseReducer from '../features/game/expertiseSlice';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import {
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+
+const persistConfig = {
+  key: 'game',
+  storage,
+};
+
+const reducers = combineReducers({
+  counter: counterReducer,
+  expertise: expertiseReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    expertise: expertiseReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware({
+      serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+  }),
 });
 
 export type AppDispatch = typeof store.dispatch;
